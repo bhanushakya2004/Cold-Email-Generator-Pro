ceca78b7f4cb76d57f82eccaa73ad34b4e29ccec
Bhanu Shakya <166703471+bhanushakya2004@users.noreply.github.com>
2025-06-09 16:30:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from langchain_community.document_loaders import WebBaseLoader
+# from langchain_community.document_loaders import WebBaseLoader
+from langchain.document_loaders import WebBaseLoader
+
 from chains import Chain
 import fitz  # PyMuPDF
 import re
